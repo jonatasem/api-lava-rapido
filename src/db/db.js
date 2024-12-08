2f126927d4cb1d4c2c3ba0af63d9d90750d52cc0
jonatasem <jonatas.em20@gmail.com>
2024-12-08 08:49:36 -0300

    construindo backend do lava rapido

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config(); // Carrega variáveis do arquivo .env
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
+    useNewUrlParser: true, // Você pode manter esta opção se necessário
+    // useUnifiedTopology: true, // Remova esta linha
 }).then(() => {
     console.log("Conectado ao MongoDB");
 }).catch(err => {
